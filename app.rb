1b3f8dc1bd828c527f125c95f8100e8e7d65e6c3
Curtis Kline <curtiskline@gmail.com>
2012-10-13 17:42:05 -0500

    more sinatra

--- app.rb
+++ app.rb
@@ -2,5 +2,16 @@ require 'sinatra'
 
 get '/' do 
   erb :index
-  
+end
+
+get '/setup' do
+  erb :setup
+end
+
+get '/reports' do
+  erb :reports
+end
+
+not_found do
+  halt 404, 'page not found'
 end
